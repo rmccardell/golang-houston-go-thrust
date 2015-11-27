8bc65e307d41abcdce3696e43fc9524b279f992f
rmccardell@imoddesigns.com <rmccardell@imoddesigns.com>
2015-11-27 16:46:17 -0600

    updating code to make models local

--- go-thrust-main.go
+++ go-thrust-main.go
@@ -21,6 +21,22 @@ import (
 	"github.com/user/gouis/go-thrust/go-thrust-meetup/models"
 )
 
+//An object that encapsulates a meet up oauth2 token response.
+//MeetUpToken...
+type MeetUpToken struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int64  `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+//Used for messaging pasing
+//godoc comment for RPCMessage
+type RPCMessage struct {
+	MessageID string
+	Content   string
+}
+
 type authInfo struct {
 	consumerKey    string
 	consumerSecret string
